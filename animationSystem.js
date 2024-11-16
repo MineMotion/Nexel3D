720e3f804dcea7e438c573825c705c6e764ae0d1
CubeMania MC <149917028+MineMotion@users.noreply.github.com>
2024-11-16 09:54:39 -0500

    Actualizar animationSystem.js

--- animationSystem.js
+++ animationSystem.js
@@ -32,10 +32,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Cambiar la imagen según el estado del autoKey
   if (autoKeyActive) {
-    autoKeyButton.innerHTML = '<img src="/icons/autokey_off.svg" alt="Auto Keyframe On">';
+    autoKeyButton.innerHTML = '<img src="icons/autokey_off.svg" alt="Auto Keyframe On">';
     autoKeyButton.style.backgroundColor = 'var(--accent-secondary)';
   } else {
-    autoKeyButton.innerHTML = '<img src="/icons/autokey_off.svg" alt="Auto Keyframe Off">';
+    autoKeyButton.innerHTML = '<img src="icons/autokey_off.svg" alt="Auto Keyframe Off">';
     autoKeyButton.style.backgroundColor = ''; 
   }
 });
@@ -54,10 +54,10 @@ document.addEventListener('DOMContentLoaded', () => {
         !selectedObject.scale.equals(currentKeyframe.scale);
 
       keyframeButton.innerHTML = hasChanged 
-        ? '<img src="/icons/keyframe_add.svg" alt="Change">'
-        : '<img src="/icons/keyframe_add.svg" alt="Add">';
+        ? '<img src="icons/keyframe_add.svg" alt="Change">'
+        : '<img src="icons/keyframe_add.svg" alt="Add">';
     } else {
-      keyframeButton.innerHTML = '<img src="/icons/keyframe.svg" alt="Add">';
+      keyframeButton.innerHTML = '<img src="icons/keyframe.svg" alt="Add">';
     }
   }
 }
@@ -262,10 +262,10 @@ document.addEventListener('DOMContentLoaded', () => {
   
   /* Pause Button */
   const pauseButton = document.getElementById('pauseButton');
-  pauseButton.innerHTML = isPaused ? '<img src="/icons/play.svg" alt="Play">' : '<img src="/icons/pause.svg" alt="Pause">';
+  pauseButton.innerHTML = isPaused ? '<img src="icons/play.svg" alt="Play">' : '<img src="icons/pause.svg" alt="Pause">';
   pauseButton.addEventListener('click', () => {
   isPaused = !isPaused;
-  pauseButton.innerHTML = isPaused ? '<img src="/icons/play.svg" alt="Play">' : '<img src="/icons/pause.svg" alt="Pause">';
+  pauseButton.innerHTML = isPaused ? '<img src="icons/play.svg" alt="Play">' : '<img src="icons/pause.svg" alt="Pause">';
 
   if (!isPaused) {
     pauseButton.style.backgroundColor = 'var(--accent-secondary)';
